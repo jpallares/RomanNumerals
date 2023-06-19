ff4b3a41c2d751bb9d90b7136c6a61cbd6012175
Juan Pallarès <juan.pallares@thetrainline.com>
2023-06-19 15:15:12 +0200

    Refactor? dictionary not needed

--- RomanNumerals.cs
+++ RomanNumerals.cs
@@ -2,33 +2,33 @@
 
 public class RomanNumerals
 {
-    private readonly Dictionary<int, string> _equivalencies = new()
+    private readonly (int, string)[] _equivalencies =
     {
-        { 1000, "M" }, 
-        { 900, "CM" }, 
-        { 500, "D" },
-        { 400, "CD" },
-        { 100, "C" },
-        { 90, "XC" },
-        { 50, "L" },
-        { 40, "XL" },
-        { 10, "X" },
-        { 9, "IX" },
-        { 5, "V" },
-        { 4, "IV" },
-        { 1, "I" }
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I")
     };
 
     public string Map(int arabic)
     {
-        string roman = "";
+        var roman = "";
         
-        foreach (var (keyArabic, valueRoman) in _equivalencies)
+        foreach (var (arabicEquivalency, romanEquivalency) in _equivalencies)
         {
-            while (arabic >= keyArabic)
+            while (arabic >= arabicEquivalency)
             {
-                roman += valueRoman;
-                arabic -= keyArabic;
+                roman += romanEquivalency;
+                arabic -= arabicEquivalency;
             }
         }
         
