a050108bda62941155c227501488aed0ec255288
Juan Pallarès <juan.pallares@thetrainline.com>
2023-06-19 14:53:21 +0200

    2 more cases

--- RomanNumerals.cs
+++ RomanNumerals.cs
@@ -4,8 +4,12 @@ public class RomanNumerals
 {
     public static string Map(int arabic)
     {
+        if (arabic == 5)
+            return "V";
+        if (arabic == 4)
+            return "IV";
+        
         string roman = "";
-
         while (arabic > 0)
         {
             roman += "I";
--- RomanNumeralsTests.cs
+++ RomanNumeralsTests.cs
@@ -8,6 +8,8 @@ public class RomanNumeralsTests
     [InlineData(1, "I")]
     [InlineData(2, "II")]
     [InlineData(3, "III")]
+    [InlineData(4, "IV")]
+    [InlineData(5, "V")]
     public void MapCorrectly(int arabic, string expectedRoman)
     {
         string result = RomanNumerals.Map(arabic);
