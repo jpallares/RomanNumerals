68b8e45f48d36797a0f8d2969c799bd179b0eb8a
noeliacar <107847979+noeliacar@users.noreply.github.com>
2023-06-21 11:14:34 +0200

    WIP

--- RomanNumerals.cs
+++ RomanNumerals.cs
@@ -2,8 +2,39 @@
 
 public class RomanNumerals
 {
-    public static string Map(int arabic)
+    private Dictionary<int, string> _romanDictionary;
+
+    public RomanNumerals()
     {
-        return "I";
+        _romanDictionary = new Dictionary<int, string>()
+        {
+            {4, "IV"},
+            {5, "V"},
+            {10, "X"}
+        };
+    }
+    public string Map(int arabic)
+    {
+        //return new string(Enumerable.Repeat('I',arabic).ToArray());
+
+        var found = _romanDictionary.TryGetValue(arabic, out var result);
+
+        if (!found || arabic > 5)
+        {
+            result = "";
+
+            if (arabic > 5)
+            {
+                result = "V";
+                arabic -= 5;
+            }
+
+            for (int i = 0; i < arabic; i++)
+            {
+                result += "I";
+            }
+        }
+
+        return result;
     }
 }
--- RomanNumeralsTests.cs
+++ RomanNumeralsTests.cs
@@ -6,9 +6,18 @@ public class RomanNumeralsTests
 {
     [Theory]
     [InlineData(1, "I")]
+    [InlineData(2, "II")]
+    [InlineData(3, "III")]
+    [InlineData(4, "IV")]
+    [InlineData(5, "V")]
+    [InlineData(6, "VI")]
+    [InlineData(7, "VII")]
+    [InlineData(8, "VIII")]
+    [InlineData(9, "IX")]
+    [InlineData(10, "X")]
     public void MapCorrectly(int arabic, string roman)
     {
-        string result = RomanNumerals.Map(arabic);
+        string result = new RomanNumerals().Map(arabic);
 
         Assert.Equal(result, roman);
     }
